5ff39291d637eeb5806087bfd5eb281f46731ad6
Ahmed Magdy <ahmed.magdy40@gmail.com>
2022-12-25 01:36:30 +0100

    [add] get conversation by id endpoint

--- src/routes.rs
+++ src/routes.rs
@@ -1,5 +1,5 @@
 use std::time::Instant;
-use actix::*;
+use actix::{*, fut::ok};
 use actix_files::NamedFile;
 use actix_web::{get,post,web, Error, HttpRequest,HttpResponse,Responder};
 use actix_web_actors::ws;
@@ -85,3 +85,28 @@ pub async fn get_user_by_id(
 }
 
 
+#[get("/conversations/{uid}")]
+pub async fn get_conversation_by_id(
+    pool: web::Data<DbPool>,
+    uid: web::Path<Uuid>,
+) -> Result<HttpResponse, Error> {
+    let room_id = uid.to_owned();
+    let conversations = web::block(move || {
+        let mut conn = pool.get()?;
+        db::get_conversation_by_room_uid(&mut conn, room_id)
+    })
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+    if let Some(data) = conversations {
+        Ok(HttpResponse::Ok().json(data))
+    } else {
+        let res = HttpResponse::NotFound().body(
+            json!({
+                "error": 404,
+                "message": format!("No conversatiokn with room_id: {room_id}")
+            })
+            .to_string(),
+        );
+        Ok(res)
+    }
+}
