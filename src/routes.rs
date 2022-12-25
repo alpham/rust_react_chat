20c12e90f7efc3cda8aa1508ac222d4bbfa395b8
Ahmed Magdy <ahmed.magdy40@gmail.com>
2022-12-25 01:27:21 +0100

    INITIAL COMMIT

---
+++ src/routes.rs
@@ -0,0 +1,87 @@
+use std::time::Instant;
+use actix::*;
+use actix_files::NamedFile;
+use actix_web::{get,post,web, Error, HttpRequest,HttpResponse,Responder};
+use actix_web_actors::ws;
+use diesel::{
+    prelude::*,
+    r2d2:{self, ConnectionError},
+};
+use serde_json::json;
+use uuid::uuid;
+use crate::db;
+use crate::models;
+use crate::server;
+use crate::session;
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+
+pub async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
+
+pub async fn chat_server(
+    req: HttpRequest, 
+    stream: web::Payload,
+    pool: web::Data<DbPool>,
+    srv: web::Data<Addr<server::ChatServer>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        session::WsChatSession {
+            id: 0,
+            hb: Instant::now(),
+            room: "main".to_string(),
+            name: None,
+            addr: srv.get_ref().clone(),
+            db_pool: pool
+        },
+        &req,
+        stream
+    )
+}
+
+
+// the rest-api
+
+#[psot("/users/create")]
+pub async fn create_user(
+    pool: web::Data<DbPool>,
+    form: web::Json<models::NewUser>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let mut conn = pool.get()?;
+        db::insert_new_user(&mut conn, &form.username, &form.phone)
+    })
+    .await?
+    .map_err(actix_web::error::ErrorUnprocessableEntity)?;
+
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[get("/users/{user_id}")]
+pub async fn get_user_by_id(
+    pool: web::Data<DbPool>,
+    id: web::Path<Uuid>,
+) -> Result<HttpResponse, Error> {
+    let user_id = id.to_owned();
+    let user = web::block(move || {
+        let mut conn = pool.get()?;
+        db::find_user_by_uid(&mut conn, user_id)
+    })
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+    if let Some(user) = user {
+        Ok(HttpResponse::Ok().json(user))
+    } else {
+        let res = HttpResponse::NotFound().body(
+            json!({
+                "error": 404,
+                "message": format!("No user found with phone: {id}")
+            })
+            .to_string(),
+        );
+        Ok(res)
+    }
+}
+
+
